37c8b5ec42ff4782e17f1538c2c9f7a22ee53474
coinscrow <hello@coinscrow.io>
2019-01-28 13:56:02 +0100

    Update recovery script

--- core/recovery.sh
+++ core/recovery.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 echo "*-*-*-- MINERSTAT OS RECOVERY --*-*-*"
+echo "*-*-*-- WAITING FOR CONNECTION --*-*-*"
+while ! ping minerstat.farm -w 1 | grep "0%"; do
+  sleep 1
+done
 sudo killall node
 sudo screen -S minerstat-console -X quit
 sudo screen -S listener -X quit
 sudo rm -rf /home/minerstat/minerstat-os
 cd /home/minerstat
 ls
-git clone http://github.com/minerstat/minerstat-os
+git clone https://github.com/minerstat/minerstat-os
 cd /home/minerstat/minerstat-os
 sudo npm install
 chmod -R 777 *
