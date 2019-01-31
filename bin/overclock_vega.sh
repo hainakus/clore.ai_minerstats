4fbc4b35b1f5a629877a52bf2979a14c2928bf3f
coinscrow <hello@coinscrow.io>
2019-01-31 03:13:20 +0100

    OC Adjustment for Vega/4

--- bin/overclock_vega.sh
+++ bin/overclock_vega.sh
@@ -27,21 +27,41 @@ if [ $1 ]; then
     VDDC=$5
 
     echo "--**--**-- GPU $1 : VEGA 56/64 --**--**--"
-  
+
     # Requirements
     sudo su -c "echo 1 > /sys/class/drm/card0/device/hwmon/hwmon0/pwm1_enable"
     sudo su -c "echo manual > /sys/class/drm/card0/device/power_dpm_force_performance_level"
     sudo su -c "echo 4 > /sys/class/drm/card0/device/pp_power_profile_mode" # Compute Mode
 
     # Core clock & VDDC
-    sudo su -c "echo 's 7 $3 $5' > /sys/class/drm/card$1/device/pp_od_clk_voltage"
+
+    if [ "$VDDC" != "0" ]
+    then
+        echo ""
+    else
+        $VDDC="1200" # DEFAULT FOR 1630Mhz @1200mV
+    fi
+
+    if [ "$VDDC" != "skip" ]
+    then
+      if [ "$CORECLOCK" != "skip" ]
+      then
+    echo "INFO: SETTING CORECLOCK > $CORECLOCK Mhz @ $VDDC mV"
+    sudo su -c "echo 's 7 $CORECLOCK $VDDC' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
     sudo su -c "echo 0 > /sys/class/drm/card0/device/pp_sclk_od"
     sudo su -c "echo 1 > /sys/class/drm/card0/device/pp_sclk_od"
+      fi
+    fi
 
     # Memory Clock
-    sudo su -c "echo 'm 3 $2 1100' > /sys/class/drm/card$1/device/pp_od_clk_voltage"
+
+    if [ "$MEMCLOCK" != "skip" ]
+    then
+    echo "INFO: SETTING MEMCLOCK > $MEMCLOCK Mhz"
+    sudo su -c "echo 'm 3 $MEMCLOCK 1100' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
     sudo su -c "echo 0 > /sys/class/drm/card0/device/pp_mclk_od"
     sudo su -c "echo 1 > /sys/class/drm/card0/device/pp_mclk_od"
+    fi
 
     # FANS
     if [ "$FANSPEED" != 0 ]
