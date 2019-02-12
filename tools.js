4dbf8de405a957bcc53db1cd262c3def00b56b33
coinscrow <hello@coinscrow.io>
2019-02-12 19:35:35 +0100

    Update tools.js

--- tools.js
+++ tools.js
@@ -559,7 +559,7 @@ module.exports = {
             fkill('zjazz_cuda').then(() => {});
             fkill('GrinProMiner').then(() => {});
             var killQuery = require('child_process').exec,
-                killQueryProc = killQuery("sudo /home/minerstat/minerstat-os/core/killport.sh", function(error, stdout, stderr) {}),
+                killQueryProc = killQuery("sudo /home/minerstat/minerstat-os/core/killport.sh", function(error, stdout, stderr) {});
         } catch (err) {}
     },
     /*
