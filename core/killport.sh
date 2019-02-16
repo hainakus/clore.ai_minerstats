07612a9a4fb086977ca01b0db5231e6adfc6cda2
coinscrow <hello@coinscrow.io>
2019-02-16 19:52:27 +0100

    killport on 3333

--- core/killport.sh
+++ core/killport.sh
@@ -9,3 +9,9 @@ fi
 for con in `sudo netstat -anp | grep $PORT | grep TIME_WAIT | awk '{print $5}'`; do
  sudo /home/minerstat/minerstat-os/core/killcx.pl $con lo
 done
+
+# This for Claymore, CryptoDredge, CCMINER-API type
+
+for con in `sudo netstat -anp | grep 3333 | awk '{print $5}'`; do
+ sudo /home/minerstat/minerstat-os/core/killcx.pl $con lo
+done
