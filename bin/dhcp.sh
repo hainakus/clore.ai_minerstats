fee77651beedc52a22cbd15055f6dece5eba45eb
coinscrow <hello@coinscrow.io>
2019-02-23 11:10:37 +0100

    Update dhcp.sh

--- bin/dhcp.sh
+++ bin/dhcp.sh
@@ -11,8 +11,17 @@ echo ""
 echo -n > /etc/network/interfaces
 echo allow-hotplug $INTERFACE  >> /etc/network/interfaces
 echo iface $INTERFACE inet dhcp  >> /etc/network/interfaces
-#echo dns-nameservers 1.1.1.1 >> /etc/network/interfaces
+
+# CloudFlare DNS
+sudo echo "" > /etc/resolv.conf
+sudo echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+sudo echo "nameserver 1.0.0.1" >> /etc/resolv.conf
+sudo echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+sudo echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+sudo resolvconf -u
+
 /etc/init.d/networking restart
+sudo su -c "systemctl restart systemd-networkd"
 
 echo ""
 TEST="$(ping google.com -w 1 | grep '1 packets transmitted')"
