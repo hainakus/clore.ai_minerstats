8e6b572a1bd39851fee9f6fa397b23b7c2668cef
coinscrow <hello@coinscrow.io>
2019-02-25 22:01:32 +0100

    Update killport.sh

--- core/killport.sh
+++ core/killport.sh
@@ -6,12 +6,12 @@ if [ ! -z "$1" ]; then
   PORT=$1
 fi
 
-for con in `sudo netstat -anp | grep $PORT | grep TIME_WAIT | awk '{print $5}'`; do
- sudo /home/minerstat/minerstat-os/core/killcx.pl $con lo
-done
-
 # This for Claymore, CryptoDredge, CCMINER-API type
 
 for con in `sudo netstat -anp | grep 3333 | awk '{print $5}'`; do
+ #sudo /home/minerstat/minerstat-os/core/killcx.pl $con lo
+done
+
+for con in `sudo netstat -anp | grep $PORT | grep TIME_WAIT | awk '{print $5}'`; do
  sudo /home/minerstat/minerstat-os/core/killcx.pl $con lo
 done
