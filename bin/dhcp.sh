6ccc1353cf4a6f0e3f83b4a4f5c4b0785f7ff570
coinscrow <hello@coinscrow.io>
2019-04-06 19:51:19 +0200

    Update dhcp.sh

--- bin/dhcp.sh
+++ bin/dhcp.sh
@@ -13,17 +13,17 @@ sudo su -c "echo allow-hotplug $INTERFACE  >> /etc/network/interfaces"
 sudo su -c "echo iface $INTERFACE inet dhcp  >> /etc/network/interfaces"
 
 # CloudFlare DNS
+sudo su -c '/etc/init.d/networking restart'
+sudo su -c "systemctl restart systemd-networkd"
+sudo ifdown $INTERFACE
+sudo ifup $INTERFACE
+
 sudo su -c 'echo "" > /etc/resolv.conf'
 sudo resolvconf -u
 sudo su -c 'echo "nameserver 1.1.1.1" >> /etc/resolv.conf'
 sudo su -c 'echo "nameserver 1.0.0.1" >> /etc/resolv.conf'
 sudo su -c 'echo "nameserver 8.8.8.8" >> /etc/resolv.conf'
 sudo su -c 'echo "nameserver 8.8.4.4" >> /etc/resolv.conf'
-
-sudo su -c '/etc/init.d/networking restart'
-sudo su -c "systemctl restart systemd-networkd"
-sudo ifdown $INTERFACE
-sudo ifup $INTERFACE
 
 echo ""
 TEST="$(ping google.com -w 1 | grep '1 packets transmitted')"
