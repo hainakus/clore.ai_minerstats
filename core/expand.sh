5fa222fb6ef2b92daacad416151a9cdb3e0e074b
coinscrow <hello@coinscrow.io>
2019-04-08 02:53:37 +0200

    Expand script almost finished for EFI

--- core/expand.sh
+++ core/expand.sh
@@ -14,13 +14,13 @@ CURRENT_PARTITION_SIZE_IN_MB=$(python -c "print $CURRENT_PARTITION_SIZE_IN_BYTE 
 #CURRENT_PARTITION_SIZE_IN_MB=$(python -c "print $CURRENT_PARTITION_SIZE_IN_GB * 1024")
 SIZE_DIFFERENCE=$(python -c "print $PARTITION_MAX_SIZE_IN_MB - $CURRENT_PARTITION_SIZE_IN_MB" | cut -f1 -d".") # 0.1 x 1000 = 100Mb
 
+echo "-*- Expanding /dev/$DRIVE_PARTITION Partition -*-"
+
 #echo $PARTITION_MAX_SIZE_IN_BYTE
 echo "Drive max size: $PARTITION_MAX_SIZE_IN_MB Mb"
 #echo $CURRENT_PARTITION_SIZE_IN_GB
 echo "Current max size: $CURRENT_PARTITION_SIZE_IN_MB"
 echo "Difference in size: $SIZE_DIFFERENCE"
-
-echo "-*- Expanding /dev/$DRIVE_PARTITION Partition -*-"
 
 # Keep 300Mb difference between drive and partition size for check
 if [ "$SIZE_DIFFERENCE" -lt "300" ]; then
@@ -34,11 +34,12 @@ if [ "$RESIZED" = "RESIZED" ]; then
 else
     if grep -q experimental "/etc/lsb-release"; then
         EFI=$(sudo cat /proc/partitions | grep "$DRIVE_EFI" | awk '{print $3}')
-        #echo $EFI
+        echo "Debug Code for EFI: $EFI"
         if [ "$EFI" = "44032" ]; then
+          echo "=== MOVING EFI PARTITION ==="
           if [ "$SIZE_DIFFERENCE" -gt "600" ]; then
 
-            LIST_FREE_SECTORS=$(echo 'F' | sudo fdisk /dev/sda)
+            LIST_FREE_SECTORS=$(echo 'F' | sudo fdisk /dev/$DRIVE_NUMBER)
             FREE_SECTORS=${LIST_FREE_SECTORS##*Size}
             FREE_SPACE_END=$(echo $FREE_SECTORS | awk '{print $2}')
             FIRST_SECTOR=$(python -c "print $FREE_SPACE_END - 88064")
@@ -46,19 +47,20 @@ else
             echo "Free Space Last Sector: $FREE_SPACE_END"
             echo "Target Sector for EFI: $FIRST_SECTOR"
 
-              if [ -z "$FIRST_SECTOR" ]; then 
+              if [ ! -z "$FIRST_SECTOR" ]; then
                 echo "Moving EFI Partition to END of the Drive"
-#               sudo umount /dev/$DRIVE_EFI
-#               (
-#                 echo d # Delete partition
-#                 echo 3 # Delete EFI
-#                 echo n # New partition
-#                 echo p # Primary
-#                 echo 3 # EFI PARTition
-#                 echo $FIRST_SECTOR # First sector (Accept default: 1)
-#                 echo   # Last sector (Accept default: varies)
-#                 echo w # Write changes
-#                ) | sudo fdisk /dev/$DRIVE_NUMBER
+                echo "Not finished yet"
+#                sudo umount /dev/$DRIVE_EFI
+#                (
+#                  echo d # Delete partition
+#                  echo 3 # Delete EFI
+#                  echo n # New partition
+#                  echo p # Primary
+#                  echo 3 # EFI PARTition
+#                  echo $FIRST_SECTOR # First sector (Accept default: 1)
+#                  echo   # Last sector (Accept default: varies)
+#                  echo w # Write changes
+#               ) | sudo fdisk /dev/$DRIVE_NUMBER
               fi
 
             fi
