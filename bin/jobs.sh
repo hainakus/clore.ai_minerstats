a1a506cce356bad49fca4c0cdeda4059b30286c9
coinscrow <hello@coinscrow.io>
2019-04-08 17:10:22 +0200

    Disable all system logs

--- bin/jobs.sh
+++ bin/jobs.sh
@@ -3,6 +3,8 @@ exec 2>/dev/null
 echo "Running Clean jobs.."
 find '/home/minerstat/minerstat-os' -name "*log.txt" -type f -delete
 sudo find /var/log -type f -name "*.journal" -delete
+sudo service rsyslog stop
+sudo systemctl disable rsyslog
 #echo "Log files deleted"
 sudo dmesg -n 1
 sudo apt clean
