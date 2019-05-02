e26e1c94708269265d01bc94cc2bf6c72ebab969
coinscrow <hello@coinscrow.io>
2019-05-02 22:12:25 +0200

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,7 +1,6 @@
 if ! screen -list | grep -q "dummy"; then
 
   screen -A -m -d -S dummy sleep 86400
-  screen -A -m -d -S fff sleep 7d
   screen -S listener -X quit # kill running process
   screen -A -m -d -S listener sudo sh /home/minerstat/minerstat-os/core/init.sh
 
