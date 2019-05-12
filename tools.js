223ab2a3474db88c52b7e670451a630a567dd65c
coinscrow <hello@coinscrow.io>
2019-05-12 15:18:29 +0200

    watchdog hook

--- tools.js
+++ tools.js
@@ -53,7 +53,7 @@ function restartNode() {
     if (global.benchmark.toString() == 'false') {
         var main = require('./start.js');
         global.watchnum++;
-        if (global.watchnum == 2 || global.watchnum == 4) {
+        if (global.watchnum == 6 || global.watchnum == 12) {
             console.log("\x1b[1;94m== \x1b[0mClient Status: \x1b[1;31mError (" + global.watchnum + "/6)\x1b[0m");
             console.log("\x1b[1;94m== \x1b[0mAction: Restarting client ...");
             clearInterval(global.timeout);
@@ -63,7 +63,7 @@ function restartNode() {
                     main.main();
                 });
         }
-        if (global.watchnum >= 6) {
+        if (global.watchnum >= 18) {
             console.log("\x1b[1;94m== \x1b[0mClient Status: \x1b[1;31mError (" + global.watchnum + "/6)\x1b[0m");
             console.log("\x1b[1;94m== \x1b[0mAction: Rebooting ...");
             clearInterval(global.timeout);
