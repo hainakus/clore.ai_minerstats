7af1c0140fafb336affbc291770f24968d465f55
coinscrow <hello@coinscrow.io>
2019-07-13 16:24:49 +0200

    Update jobs.sh

--- bin/jobs.sh
+++ bin/jobs.sh
@@ -104,6 +104,15 @@ sudo systemctl start systemd-journald.service systemd-journald.socket systemd-jo
 sudo ln -s /home/minerstat/minerstat-os/bin/jq /sbin &> /dev/null
 # Remove ppfeaturemask to avoid kernel panics with old cards
 #sudo chmod 777 /boot/grub/grub.cfg && sudo su -c "sed -Ei 's/amdgpu.ppfeaturemask=0xffffffff//g' /boot/grub/grub.cfg" && sudo chmod 444 /boot/grub/grub.cfg
+# grub fix
+GRB=/home/minerstat/minerstat-os/bin/amdmeminfo.txt
+if [ -f "$GRB" ]; then
+  if grep -q amdgpu.ppfeaturemask "/boot/grub/grub.cfg"; then
+    echo ""
+  else
+    sudo chmod 777 /boot/grub/grub.cfg && sudo su -c "sed -Ei 's/amdgpu.cik_support=1/amdgpu.cik_support=1 amdgpu.ppfeaturemask=0xffffffff/g' /boot/grub/grub.cfg" && sudo chmod 444 /boot/grub/grub.cfg
+  fi
+fi
 # Restart fan curve if running
 FNUM=$(sudo su -c "screen -list | grep -c curve")
 if [ "$FNUM" -gt "0" ]; then
