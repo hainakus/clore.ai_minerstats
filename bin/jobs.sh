7a9cc1ad298916ba751678216c5ba30f6038e185
coinscrow <hello@coinscrow.io>
2019-09-22 08:38:18 +0200

    Update jobs.sh

--- bin/jobs.sh
+++ bin/jobs.sh
@@ -38,8 +38,8 @@ sudo cp -fR /home/minerstat/minerstat-os/core/.bashrc /home/minerstat
 # rocm for VEGA
 export HSA_ENABLE_SDMA=0
 # Hugepages (XMR) [Need more test, this required or not]
-sudo su -c "echo 1168 > /proc/sys/vm/nr_hugepages"
-sudo su -c "sysctl -w vm.nr_hugepages=1168"
+sudo su -c "echo 128 > /proc/sys/vm/nr_hugepages"
+sudo su -c "sysctl -w vm.nr_hugepages=128"
 # Fix ERROR Messages
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
