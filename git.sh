2e13f49885ffca77df45693a47e72bc8c68b2348
coinscrow <hello@coinscrow.io>
2019-11-16 17:12:30 +0100

    Update git.sh

--- git.sh
+++ git.sh
@@ -27,6 +27,26 @@ then
 
 fi
 
+if grep -q "merge" /home/minerstat/debug.txt;
+then
+# Check lost
+STARTJS=$(wc -c < /home/minerstat/minerstat-os/start.js)
+WORKSH=$(wc -c < /home/minerstat/minerstat-os/bin/work.sh)
+  if [ "$STARTJS" = "0" ] || [ "$WORKSH" = "0" ]; then
+    sudo su -c "sudo screen -X -S minew quit"
+    sudo su -c "sudo screen -X -S fakescreen quit"
+    sudo su -c "screen -ls minew | grep -E '\s+[0-9]+\.' | awk -F ' ' '{print $1}' | while read s; do screen -XS $s quit; done"
+    sudo su minerstat -c "screen -X -S fakescreen quit"
+    screen -ls minerstat-console | grep -E '\s+[0-9]+\.' | awk -F ' ' '{print $1}' | while read s; do screen -XS $s quit; done
+    sudo su minerstat -c "screen -ls minerstat-console | grep -E '\s+[0-9]+\.' | awk -F ' ' '{print $1}' | while read s; do screen -XS $s quit; done"
+    sudo killall node
+    sudo su -c "cd /tmp; 
+    wget https://raw.githubusercontent.com/minerstat/minerstat-os/master/core/recovery.sh; 
+    chmod 777 recovery.sh; 
+    sh recovery.sh"
+  fi
+fi
+
 sudo rm /home/minerstat/debug.txt
 
 # APPLY NEW BASHRC
@@ -38,3 +58,4 @@ chmod -R 777 *
 # npm update
 
 sudo /home/minerstat/minerstat-os/bin/jobs.sh
+sudo sync &
