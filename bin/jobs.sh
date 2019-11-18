bdb865aece66c6e3d8e4eaf05dcf73c162fa026e
coinscrow <hello@coinscrow.io>
2019-11-18 14:02:17 +0100

    Update jobs.sh

--- bin/jobs.sh
+++ bin/jobs.sh
@@ -124,7 +124,7 @@ sudo cp /home/minerstat/minerstat-os/core/.tmux.conf /home/minerstat
 # Tmate config
 sudo cp /home/minerstat/minerstat-os/core/.tmate.conf /home/minerstat
 echo "" | ssh-keygen -N "" &> /dev/null
-sudo killall tmate
+#sudo killall tmate
 #/home/minerstat/minerstat-os/bin/tmate -S /tmp/tmate.sock new-session -d
 # Update profile
 sudo chmod 777 /etc/profile
