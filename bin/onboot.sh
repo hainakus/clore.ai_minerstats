1ba57690d042b30fc09ac794d5864a23fb86ab6a
coinscrow <hello@coinscrow.io>
2019-12-02 16:43:18 +0100

    Update onboot.sh

--- bin/onboot.sh
+++ bin/onboot.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-sudo rm /tmp/stop.pid > /dev/null 2>&1
-file=/home/minerstat/minerstat-os/bin/random.txt
-if [ -e "$file" ]; then
-  sleep 1;
-  if ! screen -list | grep -q "dummy"; then
-    echo "Moving MSOS config.js to / (LINUX)"
-    sudo cp -rf "/media/storage/config.js" "/home/minerstat/minerstat-os/"
-    screen -A -m -d -S boot_process /home/minerstat/minerstat-os/bin/work.sh
-  fi
-else
+FILE=/dev/shm/boot.pid
+if [ ! -f "$FILE" ]; then
+  sudo rm /tmp/stop.pid > /dev/null 2>&1
+  sudo su -c "echo '1' > /dev/shm/boot.pid"
+  file=/home/minerstat/minerstat-os/bin/random.txt
+  if [ -e "$file" ]; then
+    sleep 1;
+    if ! screen -list | grep -q "dummy"; then
+      echo "Moving MSOS config.js to / (LINUX)"
+      sudo cp -rf "/media/storage/config.js" "/home/minerstat/minerstat-os/"
+      screen -A -m -d -S boot_process /home/minerstat/minerstat-os/bin/work.sh
+    fi
+  else
     screen -A -m -d -S boot_process /home/minerstat/minerstat-os/bin/boot.sh
+  fi
 fi
