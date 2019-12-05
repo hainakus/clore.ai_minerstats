84ec81a59d1a240fb990b1360d69c8de7c1b54cb
coinscrow <hello@coinscrow.io>
2019-12-05 17:38:43 +0100

    Create overclock_navi.sh

---
+++ bin/overclock_navi.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#exec 2>/dev/null
+
+if [ ! $1 ]; then
+  echo ""
+  echo "--- EXAMPLE ---"
+  echo "./overclock_vega.sh 1 2 3 4 5 6"
+  echo "1 = GPUID"
+  echo "2 = Memory Clock"
+  echo "3 = Core Clock"
+  echo "4 = Fan Speed"
+  echo "5 = VDDC"
+  echo "6 = MVDD"
+  echo ""
+  echo "-- Full Example --"
+  echo "./overclock_vega.sh 0 945 1100 80 950 1070"
+  echo ""
+fi
+
+if [ $1 ]; then
+
+ #################################£
+  # Declare
+  GPUID=$1
+  MEMCLOCK=$2
+  CORECLOCK=$3
+  FANSPEED=$4
+  VDDC=$5
+  MVDD=$6
+
+  echo "--**--**-- GPU $1 : VEGA 56/64 --**--**--"
+
+  # Requirements
+  sudo su -c "echo 1 > /sys/class/drm/card$GPUID/device/hwmon/hwmon0/pwm1_enable"
+  sudo su -c "echo manual > /sys/class/drm/card$GPUID/device/power_dpm_force_performance_level"
+  sudo su -c "echo 4 > /sys/class/drm/card$GPUID/device/pp_power_profile_mode" # Compute Mode
+
+
+  # VDDC, MEMCLOCK, CORECLOCK
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  echo "Navi is not supported by Clocktune yet"
+
+   if [ "$FANSPEED" != 0 ]; then
+    FANVALUE=$(echo - | awk "{print $MAXFAN / 100 * $FANSPEED}")
+    FANVALUE=$(printf "%.0f\n" $FANVALUE)
+    echo "GPU$GPUID : FANSPEED => $FANSPEED% ($FANVALUE)"
+  else
+    FANVALUE=$(echo - | awk "{print $MAXFAN / 100 * 70}")
+    FANVALUE=$(printf "%.0f\n" $FANVALUE)
+    echo "GPU$GPUID : FANSPEED => 70% ($FANVALUE)"
+  fi
+
+  for fid in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
+    sudo su -c "echo 1 > /sys/class/drm/card$GPUID/device/hwmon/hwmon$fid/pwm1_enable" 2>/dev/null
+    sudo su -c "echo $FANVALUE > /sys/class/drm/card$GPUID/device/hwmon/hwmon$fid/pwm1" 2>/dev/null # 70%
+  done
+
+  # FANS
+  if [ "$FANSPEED" != 0 ]; then
+    sudo ./rocm-smi --setfan $FANSPEED"%"
+  else
+    sudo ./rocm-smi --setfan 70%
+  fi
+
+  # Apply
+  sudo ./rocm-smi --setsclk 7
+  sudo ./rocm-smi --setmclk 3
+  sudo su -c "echo '7' > /sys/class/drm/card$GPUID/device/pp_dpm_sclk"
+  sudo su -c "echo '3' > /sys/class/drm/card$GPUID/device/pp_dpm_mclk"
+  # Check current states
+  sudo su -c "cat /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
+  #sudo cat /sys/kernel/debug/dri/0/amdgpu_pm_info
+
+  # ECHO Changes
+  echo "-÷-*-****** CORE CLOCK *****-*-*÷-"
+  sudo su -c "cat /sys/class/drm/card$GPUID/device/pp_dpm_sclk"
+  echo "-÷-*-****** MEM  CLOCKS *****-*-*÷-"
+  sudo su -c "cat /sys/class/drm/card$GPUID/device/pp_dpm_mclk"
+  sudo su -c "echo '7' > /sys/class/drm/card$GPUID/device/pp_dpm_sclk"
+  sudo su -c "echo '3' > /sys/class/drm/card$GPUID/device/pp_dpm_mclk"
+
+  exit 1
+
+
+
+fi
