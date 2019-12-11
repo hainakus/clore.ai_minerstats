00c71d371a90515fab7b766c5560dc02ac7dcfd7
coinscrow <hello@coinscrow.io>
2019-12-11 20:51:25 +0100

    Update work.sh

--- bin/work.sh
+++ bin/work.sh
@@ -171,6 +171,9 @@ if ! screen -list | grep -q "dummy"; then
     TOKEN="$(cat /media/storage/config.js | grep 'global.accesskey' | sed 's/global.accesskey =//g' | sed 's/;//g' | sed 's/ //g' | sed 's/"//g' | sed 's/\\r//g' | sed 's/[^a-zA-Z0-9]*//g')"
     WORKER="$(cat /media/storage/config.js | grep 'global.worker' | sed 's/global.worker =//g' | sed 's/;//g' | sed 's/ //g' | sed 's/"//g' | sed 's/\\r//g')"
     HWMEMORY=$(cd /home/minerstat/minerstat-os/bin/; cat amdmeminfo.txt)
+    if [ -z "$HWMEMORY" ] || [ -f "/dev/shm/amdmeminfo.txt" ]; then
+      HWMEMORY=$(sudo cat /dev/shm/amdmeminfo.txt)
+    fi
     sudo curl --header "Content-type: application/x-www-form-urlencoded" --request POST --data "htoken=$TOKEN" --data "hworker=$WORKER" --data "hwMemory=$HWMEMORY" "https://api.minerstat.com/v2/set_node_config_os.php"
   fi
   # MCLOCK
