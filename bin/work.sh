9c46ead6d6f33d666f83513f12152c7755f59e72
coinscrow <hello@coinscrow.io>
2019-12-11 20:54:54 +0100

    Update work.sh

--- bin/work.sh
+++ bin/work.sh
@@ -174,6 +174,12 @@ if ! screen -list | grep -q "dummy"; then
     if [ -z "$HWMEMORY" ] || [ -f "/dev/shm/amdmeminfo.txt" ]; then
       HWMEMORY=$(sudo cat /dev/shm/amdmeminfo.txt)
     fi
+    if [ ! -f "/dev/shm/amdmeminfo.txt" ]; then
+      sudo /home/minerstat/minerstat-os/bin/amdmeminfo -s -o -q | tac > /dev/shm/amdmeminfo.txt &
+      sudo cp /dev/shm/amdmeminfo.txt /home/minerstat/minerstat-os/bin
+      sudo chmod 777 /home/minerstat/minerstat-os/bin/amdmeminfo.txt
+      HWMEMORY=$(sudo cat /dev/shm/amdmeminfo.txt)
+    fi
     sudo curl --header "Content-type: application/x-www-form-urlencoded" --request POST --data "htoken=$TOKEN" --data "hworker=$WORKER" --data "hwMemory=$HWMEMORY" "https://api.minerstat.com/v2/set_node_config_os.php"
   fi
   # MCLOCK
