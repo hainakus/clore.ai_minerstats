e14f132c3f51e34e0b0c3de675ba89fd0b5e2b39
coinscrow <hello@coinscrow.io>
2020-01-16 21:02:48 +0100

    Update killport.sh

--- core/killport.sh
+++ core/killport.sh
@@ -19,6 +19,14 @@ for con in `sudo netstat -anp | grep $PORT | grep TIME_WAIT | awk '{print $5}'`;
   #sudo /home/minerstat/minerstat-os/core/killcx.pl $con $interface
 done
 
-echo "Done."
+RETEST=$(sudo netstat -anp | grep $PORT 2> /dev/null | grep TIME_WAIT 2> /dev/null | awk '{print $5}' | wc -l)
 
-sleep 10
+until [ $RETEST -eq 0 ]
+do
+  sudo su -c "kill $(sudo lsof -t -i:$PORT)"
+  sudo fuser -k $PORT/tcp
+  sleep 10
+  RETEST=$(sudo netstat -anp | grep $PORT 2> /dev/null | grep TIME_WAIT 2> /dev/null | awk '{print $5}' | wc -l)
+done 
+
+echo "Done."
