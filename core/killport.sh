595661d4f170d6430defb05abf8c2863985c881a
coinscrow <hello@coinscrow.io>
2020-01-16 14:40:30 +0100

    Update killport.sh

--- core/killport.sh
+++ core/killport.sh
@@ -6,9 +6,9 @@ if [ ! -z "$1" ]; then
   PORT=$1
 fi
 
-echo "Freeing up API Port at: $PORT [$interface]";
-
 interface=$(ip addr | awk '/state UP/ {print $2}' | sed 's/.$//')
+
+echo "Freeing up API Port at: $PORT [$interface]";
 
 for con in `sudo netstat -anp | grep $PORT | grep TIME_WAIT | awk '{print $5}'`; do
   sudo /home/minerstat/minerstat-os/core/killcx.pl $con $interface
