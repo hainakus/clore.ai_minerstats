14bd8ccc4e0921375b5ed7c5d6a8cf6202daca22
coinscrow <hello@coinscrow.io>
2020-01-16 14:30:47 +0100

    Update killport.sh

--- core/killport.sh
+++ core/killport.sh
@@ -10,4 +10,5 @@ echo "Freeing up API Port at: $PORT";
 
 for con in `sudo netstat -anp | grep $PORT | grep TIME_WAIT | awk '{print $5}'`; do
   sudo /home/minerstat/minerstat-os/core/killcx.pl $con lo
+  sudo /home/minerstat/minerstat-os/core/killcx.pl $con eth0
 done
