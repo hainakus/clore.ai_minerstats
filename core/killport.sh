c4daafa1516d75575ae512c6d296334ae9fd282e
coinscrow <hello@coinscrow.io>
2020-01-17 08:44:16 +0100

    Update killport.sh

--- core/killport.sh
+++ core/killport.sh
@@ -9,7 +9,7 @@ if [ ! -z "$1" ]; then
 fi
 
 interface=$(ip addr | awk '/state UP/ {print $2}' | sed 's/.$//')
-sudo su -c "kill $(sudo lsof -t -i:$PORT)"
+#sudo su -c "kill $(sudo lsof -t -i:$PORT)"
 #sudo fuser -k $PORT/tcp
 
 echo "Freeing up API Port at: $PORT [$interface]";
@@ -19,13 +19,13 @@ for con in `sudo netstat -anp | grep $PORT | grep TIME_WAIT | awk '{print $5}'`;
   #sudo /home/minerstat/minerstat-os/core/killcx.pl $con $interface
 done
 
-if [ "$PORT" != "7888" ] && [ "$PORT" != "7887"]
+if [ "$PORT" != "4028" ] && [ "$PORT" != "7887"]
 
   RETEST=$(sudo netstat -anp | grep $PORT 2> /dev/null | grep TIME_WAIT 2> /dev/null | awk '{print $5}' | wc -l)
 
   until [ $RETEST -eq 0 ]
   do
-    sudo su -c "kill $(sudo lsof -t -i:$PORT)"
+    #sudo su -c "kill $(sudo lsof -t -i:$PORT)"
     #sudo fuser -k $PORT/tcp
     sleep 1
     RETEST=$(sudo netstat -anp | grep $PORT 2> /dev/null | grep TIME_WAIT 2> /dev/null | awk '{print $5}' | wc -l)
