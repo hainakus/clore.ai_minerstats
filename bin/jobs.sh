7837da6917059aa1ab403a1a1522ff92ee33b868
coinscrow <hello@coinscrow.io>
2020-02-10 15:46:41 +0100

    Update jobs.sh

--- bin/jobs.sh
+++ bin/jobs.sh
@@ -298,6 +298,7 @@ if [ "$version" = "1.4" ] || [ "$version" = "1.4.5" ]; then
   CHECK=$(ls /boot | grep "5.3.0" | wc - l)
   if [ "$CHECK" != "0" ]; then
     sudo rm -rf /boot/*5.3.0*
+    echo "generating new grub"
     sudo update-grub2
     sync
   fi
