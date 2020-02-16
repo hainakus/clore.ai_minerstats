32dc4d32a736c3ac86e67ca6369f4ba64cd8dfae
coinscrow <hello@coinscrow.io>
2020-02-16 17:33:46 +0100

    Update overclock_vega.sh

--- bin/overclock_vega.sh
+++ bin/overclock_vega.sh
@@ -64,10 +64,14 @@ if [ $1 ]; then
       #sudo su -c "echo 's 5 $CORECLOCK $VDDC' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
       #sudo su -c "echo 's 6 $CORECLOCK $VDDC' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
       sudo su -c "echo 's $COREINDEX $CORECLOCK $VDDC' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
-      # NITRO has 8 core state ?!
-      #sudo su -c "echo 's 8 $CORECLOCK $VDDC' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
-      #sudo su -c "echo 0 > /sys/class/drm/card$GPUID/device/pp_sclk_od"
-      #sudo su -c "echo 1 > /sys/class/drm/card$GPUID/device/pp_sclk_od"
+      sudo su -c "echo 'vc 2 $CORECLOCK $VDDC' > /sys/class/drm/card0/device/pp_od_clk_voltage"
+      sudo su -c "echo 'c' > /sys/class/drm/card0/device/pp_od_clk_voltage"
+      
+      sudo su -c "echo '0' > /sys/class/drm/card$GPUID/device/pp_sclk_od"
+      sudo su -c "echo '1' > /sys/class/drm/card$GPUID/device/pp_sclk_od"
+
+      sudo su -c "echo $COREINDEX > /sys/class/drm/card$GPUID/device/pp_dpm_sclk"
+      
     fi
   fi
 
