210b93d301125089e14ddb0908ed1261bb5549a5
coinscrow <hello@coinscrow.io>
2020-03-10 16:55:20 +0100

    Update work.sh

--- bin/work.sh
+++ bin/work.sh
@@ -87,7 +87,7 @@ if ! screen -list | grep -q "dummy"; then
 
   # Rewrite
   sudo systemctl stop systemd-resolved
-  GET_GATEWAY=$(route -n -e -4 | awk {'print $2'} | grep -vE "0.0.0.0|IP|Gateway" | head -n1 | xargs)
+  GET_GATEWAY=$(timeout 10 route -n -e -4 | awk {'print $2'} | grep -vE "0.0.0.0|IP|Gateway" | head -n1 | xargs)
   sudo su -c 'echo "" > /etc/resolv.conf' 2>/dev/null
   if [ ! -z "$GET_GATEWAY" ]; then
     sudo su -c "echo 'nameserver $GET_GATEWAY' >> /etc/resolv.conf" 2>/dev/null
