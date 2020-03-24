37a3b9b80d09a493fb72f2ad1c6b0c62f6a18dc7
coinscrow <hello@coinscrow.io>
2020-03-24 17:27:05 +0100

    Update overclock_amd.sh

--- bin/overclock_amd.sh
+++ bin/overclock_amd.sh
@@ -136,7 +136,7 @@ if [ $1 ]; then
   isThisVega=$(sudo /home/minerstat/minerstat-os/bin/amdcovc | grep "PCI $GID" | grep "Vega" | sed 's/^.*Vega/Vega/' | sed 's/[^a-zA-Z]*//g')
   isThisVegaII=$(sudo /home/minerstat/minerstat-os/bin/amdcovc | grep "PCI $GID" | grep "Vega" | sed 's/^.*Vega/Vega/' | sed 's/[^a-zA-Z]*//g')
   isThisVegaVII=$(sudo /home/minerstat/minerstat-os/bin/amdcovc | grep "PCI $GID" | grep "VII" | sed 's/^.*VII/VII/' | sed 's/[^a-zA-Z]*//g')
-  isThisNavi=$(sudo /home/minerstat/minerstat-os/bin/amdcovc | grep "PCI $GID" | grep -E "5500|5550|5600|5650|5700|5750|5800|5850|5900" | wc -l)
+  isThisNavi=$(sudo /home/minerstat/minerstat-os/bin/amdcovc | grep "PCI $GID" | grep -E "5500|5550|5600|5650|5700|5750|5800|5850|5900|5950" | wc -l)
 
   ##  echo "----"
   ##  echo $GPUBUS
