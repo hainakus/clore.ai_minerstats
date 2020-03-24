ffe8864dc86141401b3ce97244f3355c7ee34113
coinscrow <hello@coinscrow.io>
2020-03-24 16:32:03 +0100

    Update static.sh

--- bin/static.sh
+++ bin/static.sh
@@ -5,7 +5,7 @@ echo ""
 
 sudo su -c "rm /etc/netplan/minerstat.yaml"
 
-INTERFACE="$(sudo cat /proc/net/dev | grep -vE lo | tail -n1 | awk -F '\\:' '{print $1}' | xargs)"
+INTERFACE="$(sudo cat /proc/net/dev | grep -vE "lo|docker0" | tail -n1 | awk -F '\\:' '{print $1}' | xargs)"
 
 # READ FROM network.txt
 ADDRESS=$(cat /media/storage/network.txt | grep 'IPADDRESS="' | sed 's/IPADDRESS="//g' | sed 's/"//g')
