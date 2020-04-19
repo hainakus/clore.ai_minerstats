6285f9b6383505fade1291a5454c7745e2cffd8d
coinscrow <hello@coinscrow.io>
2020-04-19 19:31:39 +0200

    Update dhcp.sh

--- bin/dhcp.sh
+++ bin/dhcp.sh
@@ -1,57 +1,49 @@
 #!/bin/bash
-echo ""
-echo "*** Auto DHCP Configuration ***"
-echo ""
-
-sudo su -c "rm /etc/netplan/minerstat.yaml"
+echo -e ""
+echo -e "\033[1;34m=========== CONFIGURING DHCP ===========\033[0m"
 
 INTERFACE="$(sudo cat /proc/net/dev | grep -vE "lo|docker0" | tail -n1 | awk -F '\\:' '{print $1}' | xargs)"
 
-echo "Configuring LAN DHCP for: "$INTERFACE
-echo ""
+echo -e "\033[1;34m==\033[0m DHCP Interface: \033[1;32m$INTERFACE\033[0m"
+echo -e ""
 
-sudo su -c "echo -n > /etc/network/interfaces"
-sudo su -c "echo allow-hotplug $INTERFACE  >> /etc/network/interfaces"
-sudo su -c "echo iface $INTERFACE inet dhcp  >> /etc/network/interfaces"
+NSCHECK=$(cat /etc/resolv.conf | grep 'nameserver 1.1.1.1' | xargs)
+if [ "$NSCHECK" != "nameserver 1.1.1.1" ]; then
+  sudo su -c 'echo -n > /etc/resolv.conf; echo -e "nameserver 1.1.1.1" >> /etc/resolv.conf; echo -e "nameserver 1.0.0.1" >> /etc/resolv.conf; echo -e "nameserver 8.8.8.8" >> /etc/resolv.conf; echo -e "nameserver 8.8.4.4" >> /etc/resolv.conf; echo -e "nameserver 114.114.114.114" >> /etc/resolv.conf; echo -e "nameserver 114.114.115.115" >> /etc/resolv.conf; echo nameserver 2606:4700:4700::1111 >> /etc/resolv.conf; echo nameserver 2606:4700:4700::1001 >> /etc/resolv.conf;' 2>/dev/null
+fi
 
-# CloudFlare DNS
-sudo su -c '/etc/init.d/networking restart'
-sudo su -c "systemctl restart systemd-networkd"
-sudo ifdown $INTERFACE
-sudo nohup ifup $INTERFACE &
+NETRESTART="NO"
+IFACE=$(cat /etc/network/interfaces | grep "allow-hotplug $INTERFACE" | xargs)
+if [ "$IFACE" != "allow-hotplug $INTERFACE" ]; then
+  sudo su -c "echo -n > /etc/network/interfaces; echo allow-hotplug $INTERFACE >> /etc/network/interfaces" 2>/dev/null
+  NETRESTART="YES"
+fi
 
-sudo su -c 'echo "" > /etc/resolv.conf' 2>&1 >/dev/null
-#sudo resolvconf -u
-sudo su -c 'echo "nameserver 1.1.1.1" >> /etc/resolv.conf' 2>&1 >/dev/null
-sudo su -c 'echo "nameserver 1.0.0.1" >> /etc/resolv.conf' 2>&1 >/dev/null
-sudo su -c 'echo "nameserver 8.8.8.8" >> /etc/resolv.conf' 2>&1 >/dev/null
-sudo su -c 'echo "nameserver 8.8.4.4" >> /etc/resolv.conf' 2>&1 >/dev/null
-# China
-sudo su -c 'echo "nameserver 114.114.114.114" >> /etc/resolv.conf' 2>&1 >/dev/null
-sudo su -c 'echo "nameserver 114.114.115.115" >> /etc/resolv.conf' 2>&1 >/dev/null
-# For msos versions what have local DNS cache
-#sudo su -c 'echo "nameserver 127.0.0.1" >> /etc/resolv.conf' 2>&1 >/dev/null
-# IPV6
-sudo su -c 'echo nameserver 2606:4700:4700::1111 >> /etc/resolv.conf' 2>&1 >/dev/null
-sudo su -c 'echo nameserver 2606:4700:4700::1001 >> /etc/resolv.conf' 2>&1 >/dev/null
-# systemd resolve casusing problems with 127.0.0.53
-sudo su -c 'echo "nameserver 1.1.1.1" > /run/resolvconf/interface/systemd-resolved' 2>&1 >/dev/null
-sudo su -c 'echo "nameserver 1.0.0.1" >> /run/resolvconf/interface/systemd-resolved' 2>&1 >/dev/null
-sudo su -c 'echo "nameserver 1.1.1.1" > /run/systemd/resolve/stub-resolv.conf' 2>&1 >/dev/null
-sudo su -c 'echo "nameserver 1.0.0.1" >> /run/systemd/resolve/stub-resolv.conf' 2>&1 >/dev/null
-sudo su -c 'echo options edns0 >> /run/systemd/resolve/stub-resolv.conf' 2>&1 >/dev/null
+IFACE=$(cat /etc/network/interfaces | grep "iface $INTERFACE inet dhcp" | xargs)
+if [ "$IFACE" != "iface $INTERFACE inet dhcp" ]; then
+  sudo su -c "echo iface $INTERFACE inet dhcp >> /etc/network/interfaces" 2>/dev/null
+  NETRESTART="YES"
+fi
 
-echo ""
-sleep 3
-TEST="$(ping api.minerstat.com -w 1 | grep '1 packets transmitted')"
+IFACESTATUS=$(sudo /sbin/ifconfig $INTERFACE | grep "UP," | xargs)
+if [[ "$IFACESTATUS" != *"UP,"* ]]; then
+  sudo ifdown $INTERFACE
+  sudo nohup ifup $INTERFACE &
+  sleep 4
+fi
 
-if echo "$TEST" | grep "0%" ;then
-  echo ""
-  echo "Success! You have active internet connection."
+TEST="$(ping api.minerstat.com -w 1 | grep '1 packets transmitted' | xargs)"
+if [[ "$TEST" == *"0% packet loss"* ]]; then
+  echo -e ""
+  echo -e "\033[1;34m==\033[0m Internet connection: \033[1;32mONLINE\033[0m"
 else
-  echo ""
-  echo "Oh! Something went wrong, you are not connected to the internet."
-  sudo su -c "systemctl restart systemd-networkd"
+  echo -e ""
+  echo -e "\033[1;34m==\033[0m Internet connection: \033[1;31mOFFLINE\033[0m"
+  NETRESTART="YES"
+fi
+
+if [ "$NETRESTART" = "YES" ]; then
+  sudo su -c "/etc/init.d/networking restart; systemctl restart systemd-networkd" 2>/dev/null
 fi
 
 if [ "$INTERFACE" = "eth0" ]; then
@@ -67,7 +59,3 @@ if [ "$INTERFACE" = "eth0" ]; then
   sudo echo "         addresses: [1.1.1.1, 1.0.0.1]" >> /etc/netplan/minerstat.yaml
   sudo /usr/sbin/netplan apply
 fi
-
-echo ""
-echo "*** https://minerstat.com ***"
-echo ""
