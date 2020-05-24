859df2c1f5ab0a81429947aeeb311e1c92ee37f8
coinscrow <info@minerstat.com>
2020-05-24 11:35:10 +0000

    Update overclock_vega7.sh

--- bin/overclock_vega7.sh
+++ bin/overclock_vega7.sh
@@ -77,11 +77,7 @@ if [ $1 ]; then
 
   if [ "$VDDC" != "skip" ]; then
     if [ "$CORECLOCK" != "skip" ]; then
-      #sudo su -c "echo 's 0 808 $VDDC' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage" # This belongs to Mem
       sudo su -c "echo 's 1 $CORECLOCK' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
-      sudo su -c "echo 's 1 $CORECLOCK $VDDC' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
-      #sudo su -c "echo 's 2 991 $VDDC' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage" # This belongs to Mem
-      #sudo su -c "echo 'vc 0 808 $VDDC' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
       #sudo su -c "echo 'vc 1 $CORECLOCK $VDDC' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
       sudo su -c "echo 'vc 2 $CORECLOCK $VDDC' > /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
 
@@ -96,8 +92,6 @@ if [ $1 ]; then
 
     fi
   fi
-  
-  echo "Debug: MVDD: $MVDD, VDDC: $VDDC, GPUID: $GPUID, mem: $MEMCLOCK - core: $CORECLOCK | index: $COREINDEX"
 
   # Apply Changes
   sudo su -c "echo 'c'> /sys/class/drm/card$GPUID/device/pp_od_clk_voltage"
