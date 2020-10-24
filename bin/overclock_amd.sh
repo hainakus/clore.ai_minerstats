e21c242a5e4c6d22159b78a17fbddd6ce1e124dd
coinscrow <info@minerstat.com>
2020-10-25 00:01:06 +0200

    check above 10gpu

--- bin/overclock_amd.sh
+++ bin/overclock_amd.sh
@@ -141,7 +141,7 @@ if [ $1 ]; then
     GID=""
     if [ -f "/dev/shm/amdmeminfo.txt" ]; then
       echo "no bus id but cache found, detecting $GPUID"
-      FETCH=$(cat /dev/shm/amdmeminfo.txt | grep "GPU$GPUID")
+      FETCH=$(cat /dev/shm/amdmeminfo.txt | grep "GPU$GPUID:")
       isThisVegaVII=$(echo $FETCH | grep -E "VII" | wc -l)
       isThisNavi=$(echo $FETCH | grep -E "5500|5550|5600|5650|5700|5750|5800|5850|5900|5950|6600|6700|6800|6900" | wc -l)
       if [[ "$isThisVegaVII" -gt 0 ]] && [[ "$isThisNavi" -gt 0 ]]; then
