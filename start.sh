9461b40fbac5ced4cccba05a9cfb36838eb0910e
coinscrow <info@minerstat.com>
2020-12-07 20:11:52 +0100

    fix tmux issue on newer kernels

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 #echo "Freeing up RAM. Please wait..";
 #sync; sudo su -c "echo 1 > /proc/sys/vm/drop_caches"
+# can't create socket: Permission denied
+sudo rm -r /tmp/tmux-*
 sudo su -c "sudo screen -X -S minew quit"
 sudo su -c "sudo screen -X -S fakescreen quit"
 sudo su minerstat -c "screen -X -S fakescreen quit"
