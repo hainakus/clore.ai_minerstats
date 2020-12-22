ff4d2383181d4ad239e80481c4581356cfdb4887
coinscrow <info@minerstat.com>
2020-12-22 21:40:23 +0100

    fix emer log

--- bin/reboot.sh
+++ bin/reboot.sh
@@ -7,16 +7,17 @@ sudo echo 1 > /proc/sys/kernel/sysrq #enable sysrq
 sudo killall watchdog >/dev/null 2>&1
 
 # sync, unmount, reboot
+RAMLOG=""
+RAMLOG=$(timeout 5 cat /dev/shm/miner.log | tac | head --lines 10 | tac)
 sudo echo s > /proc/sysrq-trigger #(*S*nc) Sync all cached disk operations to disk
 sudo echo u > /proc/sysrq-trigger #(*U*mount) Umounts all mounted partitions
 
 # attempt
-RAMLOG=""
+
 TOKEN="$(cat /media/storage/config.js | grep 'global.accesskey' | sed 's/global.accesskey =//g' | sed 's/;//g' | sed 's/ //g' | sed 's/"//g' | sed 's/\\r//g' | sed 's/[^a-zA-Z0-9]*//g')"
 WORKER="$(cat /media/storage/config.js | grep 'global.worker' | sed 's/global.worker =//g' | sed 's/;//g' | sed 's/ //g' | sed 's/"//g' | sed 's/\\r//g')"
 
 function reboots () {
-  RAMLOG=$(timeout 5 cat /dev/shm/miner.log | tac | head --lines 10 | tac)
   RAMLOG="$RAMLOG - System reboot"
   timeout 4 sudo curl --insecure --connect-timeout 2 --max-time 3 --retry 0 --header "Content-type: application/x-www-form-urlencoded" --request POST --data "htoken=$TOKEN" --data "hworker=$WORKER" --data "mineLog=$RAMLOG" "https://api.minerstat.com:2053/v2/set_node_config_os2.php"
   # Is octo or minerdude board ?
@@ -30,7 +31,6 @@ function reboots () {
 }
 
 function sdown () {
-  RAMLOG=$(timeout 5 cat /dev/shm/miner.log | tac | head --lines 10 | tac)
   RAMLOG="$RAMLOG - System shutdown"
   timeout 4 sudo curl --insecure --connect-timeout 2 --max-time 3 --retry 0 --header "Content-type: application/x-www-form-urlencoded" --request POST --data "htoken=$TOKEN" --data "hworker=$WORKER" --data "mineLog=$RAMLOG" "https://api.minerstat.com:2053/v2/set_node_config_os2.php"
   if [[ -f "/dev/shm/octo.pid" ]]; then
@@ -58,7 +58,6 @@ if [[ "$1" = "safeshutdown" ]]; then
 fi
 
 if [[ "$1" = "powercycle" ]]; then
-  RAMLOG=$(timeout 5 cat /dev/shm/miner.log | tac | head --lines 10 | tac)
   RAMLOG="$RAMLOG - System PowerCycle"
   timeout 4 sudo curl --insecure --connect-timeout 2 --max-time 3 --retry 0 --header "Content-type: application/x-www-form-urlencoded" --request POST --data "htoken=$TOKEN" --data "hworker=$WORKER" --data "mineLog=$RAMLOG" "https://api.minerstat.com:2053/v2/set_node_config_os2.php"
   # Check it is supported, if not use normal reboot
