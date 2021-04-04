b48a276e5da377e4ca8069d9ee17057cc99ba299
coinscrow <info@minerstat.com>
2021-04-04 18:03:16 +0200

    wifi up

--- core/wifi.sh
+++ core/wifi.sh
@@ -61,6 +61,14 @@ if echo "$DEVICE" | grep "w" ;then
     sudo su -c 'echo -e "" > /etc/resolv.conf; echo -e "nameserver 1.1.1.1" >> /etc/resolv.conf; echo -e "nameserver 1.0.0.1" >> /etc/resolv.conf; echo -e "nameserver 8.8.8.8" >> /etc/resolv.conf; echo -e "nameserver 8.8.4.4" >> /etc/resolv.conf; echo -e "nameserver 114.114.114.114" >> /etc/resolv.conf; echo -e "nameserver 114.114.115.115" >> /etc/resolv.conf; echo nameserver 2606:4700:4700::1111 >> /etc/resolv.conf; echo nameserver 2606:4700:4700::1001 >> /etc/resolv.conf; echo -e "nameserver 1.1.1.1" > /run/resolvconf/interface/systemd-resolved; echo -e "nameserver 1.0.0.1" >> /run/resolvconf/interface/systemd-resolved; echo -e "nameserver 1.1.1.1" > /run/systemd/resolve/stub-resolv.conf; echo -e "nameserver 1.0.0.1" >> /run/systemd/resolve/stub-resolv.conf; echo options edns0 >> /run/systemd/resolve/stub-resolv.conf' 2>/dev/null
   fi
 
+  # try to up
+  IFACESTATUS=$(sudo timeout 5 /sbin/ifconfig $DEVICE | grep "UP," | xargs)
+  if [[ "$IFACESTATUS" != *"UP,"* ]]; then
+    #sudo ifconfig $INTERFACE down
+    nohup sudo ifconfig $INTERFACE up &
+    sleep 4
+  fi
+
   TEST="$(ping api.minerstat.com -w 1 | grep '1 packets transmitted' | xargs)"
   if [[ "$TEST" == *"0% packet loss"* ]]; then
     echo ""
@@ -68,6 +76,8 @@ if echo "$DEVICE" | grep "w" ;then
   else
     echo ""
     echo -e "\033[1;34m==\033[0m Internet connection: \033[1;31mOFFLINE\033[0m"
+    echo ""
+    nmcli device wifi connect "$SSID" password "$PASSWD"
   fi
 
 else
