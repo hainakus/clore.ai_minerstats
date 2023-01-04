0f2df747896fed131ca9830018580d3595531464
minerstat <dump@minerstat.com>
2023-01-04 19:32:10 +0200

    Init

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
-global.accesskey = "CHANGEIT";
-global.worker = "CHANGEIT";
-global.path = __dirname;
+//
+// You can get access key by registering on https://my.minerstat.com/register
+//
+// DO NOT REMOVE THE QUOTES!
+
+// If your accesskey is xKo934Onk then you need to use in this way: "xKo934Onk";
+
+global.accesskey = "CHANGEME";
+global.worker = "CHANGEME";
