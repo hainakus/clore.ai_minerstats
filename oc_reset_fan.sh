0b507d95db2dac02eda123179b696e6aa807a5c9
hainakus <djhainakosurge@gmail.com>
2023-01-07 00:23:01 +0000

    Docker conf

--- oc_reset_fan.sh
+++ oc_reset_fan.sh
@@ -1,7 +1,7 @@
 
 for i in "$@"; do
 nvidia-smi -i $i -pl 280
-sudo nvidia-settings -a GPUFanControlState=1 -a GPUTargetFanSpeed=30 
-sudo nvidia-settings -a "[gpu:$i]/GPUFanControlState=1" -a "[gpu:$i]/GPUGraphicsClockOffset[3]=0" -a "[gpu:$i]/GPUMemoryTransferRateOffset[3]=0"
+ nvidia-settings -a GPUFanControlState=1 -a GPUTargetFanSpeed=30
+ nvidia-settings -a "[gpu:$i]/GPUFanControlState=1" -a "[gpu:$i]/GPUGraphicsClockOffset[3]=0" -a "[gpu:$i]/GPUMemoryTransferRateOffset[3]=0"
 echo $i
 done
